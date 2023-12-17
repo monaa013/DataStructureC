c8de34b8d0c2e4f4a8c55ecbf1851851265fc025
Priya Dharshini G <139952619+monaa013@users.noreply.github.com>
2023-12-17 16:37:47 +0530

    Add files via upload
    Implement linked list and its operations
    Consider each node as structure representation of data  for Chocolate Quality checking. Perform all operations and implement different types of linked list.

---
+++ LinkedListLab2.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertAtBeginning(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    newNode->next = *head;
+    *head = newNode;
+}
+
+void insertAtEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    struct Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void deleteNode(struct Node** head, int key) {
+    struct Node *temp = *head, *prev = NULL;
+    if (temp != NULL && temp->data == key) {
+        *head = temp->next;
+        free(temp);
+        return;
+    }
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+    if (temp == NULL) {
+        printf("Node with value %d not found\n", key);
+        return;
+    }
+    prev->next = temp->next;
+    free(temp);
+}
+
+void displayList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+void freeList(struct Node** head) {
+    struct Node* current = *head;
+    struct Node* next;
+    while (current != NULL) {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+    *head = NULL;
+}
+
+int main() {
+    struct Node* head = NULL;
+    int choice, data;
+
+    do {
+        printf("\n1. Insert at the beginning\n");
+        printf("2. Insert at the end\n");
+        printf("3. Delete a node\n");
+        printf("4. Display the list\n");
+        printf("0. Quit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter data to insert at the beginning: ");
+                scanf("%d", &data);
+                insertAtBeginning(&head, data);
+                break;
+            case 2:
+                printf("Enter data to insert at the end: ");
+                scanf("%d", &data);
+                insertAtEnd(&head, data);
+                break;
+            case 3:
+                printf("Enter the value of the node to delete: ");
+                scanf("%d", &data);
+                deleteNode(&head, data);
+                break;
+            case 4:
+                printf("Linked list: ");
+                displayList(head);
+                break;
+            case 0:
+                printf("Exiting the program\n");
+                break;
+            default:
+                printf("Invalid choice\n");
+        }
+
+    } while (choice != 0);
+
+    // Free the memory occupied by the linked list before exiting
+    freeList(&head);
+
+    return 0;
+}
